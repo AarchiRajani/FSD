6c48a15bb2cd65b97942f4bd84ec9e0229f809ec
Aarchi <21dcs103@charusat.edu.in>
2023-07-12 10:02:27 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+
+import './App.css';
+import Home from './Components/Home/Home';
+import Contact from './Components/Contact/Contact';
+import About from './Components/About/About';
+import {BrowserRouter,Route,Routes} from 'react-router-dom';
+
+function App() {
+  return (
+    <div>
+    <BrowserRouter>
+    <Routes>
+    <Route exact path="/" element={<Home/>}/>
+    <Route exact path="/about" element={<About/>}/>
+    <Route exact path="contact" element={<Contact/>}/>
+    </Routes>
+    </BrowserRouter>
+    </div>
+
+  );
+}
+
+export default App;
